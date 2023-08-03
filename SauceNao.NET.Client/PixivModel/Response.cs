022cd8879115104909a90b098f84b497059115ba
Chik3r <31808958+Chik3r@users.noreply.github.com>
2023-08-02 20:14:45 -0400

    create pixiv data models

---
+++ SauceNao.NET.Client/PixivModel/Response.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SauceNao.NET.Client.PixivModel; 
+
+public record Root(
+    [property: JsonPropertyName("error")] bool Error,
+    [property: JsonPropertyName("message")] string Message,
+    [property: JsonPropertyName("body")] Illustration Body
+);
---
+++ SauceNao.NET.Client/PixivModel/Urls.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SauceNao.NET.Client.PixivModel; 
+
+public record Urls(
+    [property: JsonPropertyName("mini")] string Mini,
+    [property: JsonPropertyName("thumb")] string Thumb,
+    [property: JsonPropertyName("small")] string Small,
+    [property: JsonPropertyName("regular")] string Regular,
+    [property: JsonPropertyName("original")] string Original
+);
